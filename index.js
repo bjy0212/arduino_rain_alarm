92e4475de276ee7ae4ffbe8fb73aa576b2e7988f
bjy000212 <bjy0212@gmail.com>
2020-11-07 19:07:09 +0900

    some changes

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ app.post("/data", (req, res) => {
 	//get rain sensor data from arduino and store it in storage
 	const id = crypto.scryptSync(req.headers.secret, req.headers.id, 64, { N: 1024 }).toString("hex");
     let rain = req.body.rain;
+
+    //returns prev_rain data which could be 0, 1, 2
+    /*
+     * 0: 비가 오지 않음
+     * 1: 비가 내리는 중
+     * 2: 비가 오다가 멈춘 상태
+    */
 });
 
 app.post("/sync", (req, res) => {
