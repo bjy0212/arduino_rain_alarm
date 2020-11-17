1148663726b772f945b29ae784812ecbd313b76c
bjy0212 <bjy0212@gmail.com>
2020-11-17 05:36:37 +0000

    dond

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ if(!fs.existsSync(__dirname + "/storage/")) {
 	fs.writeFileSync(__dirname + "/storage/db.json", "{}");
 }
 
-if(!fs.existsSync(__dirname + "/storage/LED")) {
-	fs.mkdirSync(__dirname + "/storage/LED");
+if(!fs.existsSync(__dirname + "/storage/rain")) {
 	fs.mkdirSync(__dirname + "/storage/rain");
 }
 
 app.get("/", (req, res) => {
-	res.send("hello world");
-})
+	res.send("*h");
+});
 
 app.post("/data", (req, res) => {
+	console.log("*data added to " + req.headers.id);
 	if(!Certificate(req.headers.id, req.headers.secret)) {
-		res.json({status: 1, message: "Forbidden: id or secret is wrong"});
+		res.send("*e");
 		return;
 	}
 	//get rain sensor data from arduino and store it in storage
     let data = req.headers.rain * 1;
 	if(data === undefined) {
-		res.json({status: 2, message: "Forbidden: header 'rain' is needed"});
+		res.send("*e");
 		return;
 	}
 
@@ -40,24 +40,13 @@ app.post("/data", (req, res) => {
 
 	let rained = Manage(data, req.headers.id, dates, times) * 1;
 
-	res.json({
-		status: 200,
-		message: "ok",
-		prev: rained
-	});
-
-	RaintoLED(data, req.headers.id, dates, times);
-    //returns prev_rain data which could be 0, 1, 2
-    /*
-     * 0: 비가 오지 않음
-     * 1: 비가 내리는 중
-     * 2: 비가 오다가 멈춘 상태
-    */
+	res.send("*"+rained);
 });
 
 app.post("/sync", (req, res) => {
+	console.log("*sync requested for " + req.headers.id);
 	if(!Certificate(req.headers.id, req.headers.secret)) {
-		res.json({status: 1, message: "Forbidden: id or secret is wrong"});
+		res.send("*e");
 		return;
 	}
 
@@ -69,30 +58,21 @@ app.post("/sync", (req, res) => {
 
 	let db = [];
 
-	if(!fs.existsSync(__dirname + "/storage/LED/" + req.headers.id + "/" + dates + ".json")) {
-		const dir = fs.readdirSync(__dirname + "/storage/LED/");
-		if(!dir.length === 0) db = JSON.parse(fs.readFileSync(__dirname + "/storage/LED/" + req.headers.id + "/" + dir[dir.length - 1]));
+	if(!fs.existsSync(__dirname + "/storage/rain/" + req.headers.id + "/" + dates + ".json")) {
+		const dir = fs.readdirSync(__dirname + "/storage/rain/");
+		if(!dir.length === 0) db = JSON.parse(fs.readFileSync(__dirname + "/storage/rain/" + req.headers.id + "/" + dir[dir.length - 1]));
 	} else {
-		db = JSON.parse(fs.readFileSync(__dirname + "/storage/LED/" + req.headers.id + "/" + dates + ".json"));
+		db = JSON.parse(fs.readFileSync(__dirname + "/storage/rain/" + req.headers.id + "/" + dates + ".json"));
 	}
 
 	if(db.length === 0) {
-		res.json({
-			status: 3,
-			message: "Forbidden: no data in the storage"
-		});
+		res.send("*m");
 		return;
 	}
 
-	const led = db[db.length - 1];
+	const led = db[db.length - 1].data;
 
-	res.json({
-		status: 200,
-		message: "ok",
-		red: led.red,
-		green: led.green,
-		blue: led.blue
-	});
+	res.send("*" + led);
 });
 
 function Certificate(id, secret) {
@@ -100,7 +80,6 @@ function Certificate(id, secret) {
 	const db = JSON.parse(fs.readFileSync(__dirname + "/storage/db.json"));
 	if(!db[id]) {
 		db[id] = code;
-		fs.mkdirSync(__dirname + "/storage/LED/" + id + "/");
 		fs.mkdirSync(__dirname + "/storage/rain/" + id + "/");
 		fs.writeFileSync(__dirname + "/storage/db.json", JSON.stringify(db));
 		console.log(id + " added");
@@ -119,19 +98,7 @@ function Manage(data, id, dates, times) {
 
 	let len = db.length;
 	if(len === 1) return db[0].data;
-	return db[len - 1].data === 0 ? (db[len - 2].data === 0 ? 0 : 2) : 1;
-}
-
-function RaintoLED(data, id, dates, times) {
-	//change data in storage to color codes
-	if(!fs.existsSync(__dirname + "/storage/LED/" + id + "/" + dates + ".json")) fs.writeFileSync(__dirname + "/storage/LED/" + id + "/" + dates + ".json", "[]");
-	const db = JSON.parse(fs.readFileSync(__dirname + "/storage/LED/" + id + "/" + dates + ".json"));
-
-	const led = data === 0 ? {red: 0, green: 84, blue: 255} : {red: 255, green: 25, blue: 0};
-	led.time = times;
-	db.push(led);
-
-	fs.writeFileSync(__dirname + "/storage/LED/" + id + "/" + dates + ".json", JSON.stringify(db));
+	return db[len - 1].data;
 }
 
 function two(n) {
